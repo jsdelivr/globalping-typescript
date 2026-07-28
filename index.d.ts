2e496395a7895067eac37489a016a768f1eda75e
Martin Kolárik <martin@kolarik.sk>
2026-07-28 19:37:16 +0200

    fix: bundle CJS declarations with Rollup

---
+++ index.d.ts
@@ -0,0 +1,4 @@
+export as namespace globalping;
+
+export * from './dist/esm/index.js';
+export { default } from './dist/esm/index.js';
--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,38 @@
 import json from '@rollup/plugin-json';
 import nodeResolve from '@rollup/plugin-node-resolve';
+import { dts } from 'rollup-plugin-dts';
 
-export default {
-	input: 'dist/esm/index.js',
-	output: [
-		{
-			file: 'dist/cjs/bundle.cjs',
-			format: 'cjs',
-			exports: 'named',
-			sourcemap: true,
+export default [
+	{
+		input: 'dist/esm/index.js',
+		output: [
+			{
+				file: 'dist/cjs/bundle.cjs',
+				format: 'cjs',
+				exports: 'named',
+				sourcemap: true,
+			},
+			{
+				name: 'globalping',
+				file: 'dist/umd/bundle.js',
+				format: 'umd',
+				exports: 'named',
+				sourcemap: true,
+			},
+		],
+		plugins: [
+			json(),
+			nodeResolve(),
+		],
+	},
+	{
+		input: 'dist/esm/index.d.ts',
+		output: {
+			file: 'dist/cjs/bundle.d.cts',
+			format: 'es',
 		},
-		{
-			name: 'globalping',
-			file: 'dist/umd/bundle.js',
-			format: 'umd',
-			exports: 'named',
-			sourcemap: true,
-		},
-	],
-	plugins: [
-		json(),
-		nodeResolve(),
-	],
-};
+		plugins: [
+			dts(),
+		],
+	},
+];
